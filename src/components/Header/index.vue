a67e60a69913569cae819c78ddae14cc724c3b22
msy <805109058@qq.com>
2023-02-09 16:58:02 +0800

    路由传参

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -32,8 +32,8 @@
             </h1>
             <div class="searchArea">
                 <form action="###" class="searchForm">
-                    <input type="text" id="autocomplete" class="input-error input-xxlarge" v-model="keyword"/>
-                    <button class="sui-btn btn-xlarge btn-danger" type="button">搜索</button>
+                    <input type="text" id="autocomplete" class="input-error input-xxlarge" v-model="keyword" />
+                    <button class="sui-btn btn-xlarge btn-danger" type="button" @click="goSearch">搜索</button>
                 </form>
             </div>
         </div>
@@ -42,111 +42,118 @@
 
 <script>
 export default {
-    name:'Header',
+    name: 'Header',
     data() {
-    return {
-        keyword:''
-    }
-},
+        return {
+            keyword: ''
+        }
+    },
+    methods: {
+        goSearch(){
+            // this.$router.push("/search/"+this.keyword+"?k="+this.keyword.toUpperCase())
+            // this.$router.push(`/search/${this.keyword}?k=${this.keyword.toUpperCase()}`)
+            // this.$router.push({name:"search",params:{keyword:this.keyword},query:{k:this.keyword.toUpperCase()}})
+            this.$router.push({name:"search",params:{keyword:this.keyword},query:{k:this.keyword.toUpperCase()}})
+        }
+    },
 }
 
 </script>
 
 <style scoped lang="less">
+.header {
+    &>.top {
+        background-color: #eaeaea;
+        height: 30px;
+        line-height: 30px;
 
-    .header {
-        &>.top {
-            background-color: #eaeaea;
-            height: 30px;
-            line-height: 30px;
-
-            .container {
-                width: 1200px;
-                margin: 0 auto;
-                overflow: hidden;
-
-                .loginList {
-                    float: left;
-
-                    p {
-                        float: left;
-                        margin-right: 10px;
-
-                        .register {
-                            border-left: 1px solid #b3aeae;
-                            padding: 0 5px;
-                            margin-left: 5px;
-                        }
-                    }
-                }
-
-                .typeList {
-                    float: right;
-
-                    a {
-                        padding: 0 10px;
-
-                        &+a {
-                            border-left: 1px solid #b3aeae;
-                        }
-                    }
-
-                }
-
-            }
-        }
-
-        &>.bottom {
+        .container {
             width: 1200px;
             margin: 0 auto;
             overflow: hidden;
 
-            .logoArea {
+            .loginList {
                 float: left;
 
-                .logo {
-                    img {
-                        width: 175px;
-                        margin: 25px 45px;
+                p {
+                    float: left;
+                    margin-right: 10px;
+
+                    .register {
+                        border-left: 1px solid #b3aeae;
+                        padding: 0 5px;
+                        margin-left: 5px;
                     }
                 }
             }
 
-            .searchArea {
+            .typeList {
                 float: right;
-                margin-top: 35px;
 
-                .searchForm {
-                    overflow: hidden;
+                a {
+                    padding: 0 10px;
 
-                    input {
-                        box-sizing: border-box;
-                        width: 490px;
-                        height: 32px;
-                        padding: 0px 4px;
-                        border: 2px solid #ea4a36;
-                        float: left;
-
-                        &:focus {
-                            outline: none;
-                        }
+                    &+a {
+                        border-left: 1px solid #b3aeae;
                     }
+                }
 
-                    button {
-                        height: 32px;
-                        width: 68px;
-                        background-color: #ea4a36;
-                        border: none;
-                        color: #fff;
-                        float: left;
-                        cursor: pointer;
+            }
 
-                        &:focus {
-                            outline: none;
-                        }
+        }
+    }
+
+    &>.bottom {
+        width: 1200px;
+        margin: 0 auto;
+        overflow: hidden;
+
+        .logoArea {
+            float: left;
+
+            .logo {
+                img {
+                    width: 175px;
+                    margin: 25px 45px;
+                }
+            }
+        }
+
+        .searchArea {
+            float: right;
+            margin-top: 35px;
+
+            .searchForm {
+                overflow: hidden;
+
+                input {
+                    box-sizing: border-box;
+                    width: 490px;
+                    height: 32px;
+                    padding: 0px 4px;
+                    border: 2px solid #ea4a36;
+                    float: left;
+
+                    &:focus {
+                        outline: none;
+                    }
+                }
+
+                button {
+                    height: 32px;
+                    width: 68px;
+                    background-color: #ea4a36;
+                    border: none;
+                    color: #fff;
+                    float: left;
+                    cursor: pointer;
+
+                    &:focus {
+                        outline: none;
                     }
                 }
             }
         }
     }
+}
 </style>
--- src/pages/Search/index.vue
+++ src/pages/Search/index.vue
@@ -1,5 +1,8 @@
 <template>
-  
+  <div>
+    <h2>params参数： {{ $route.params.keyword }}</h2>
+    <h2>query参数：  {{$route.query.k }}</h2>
+  </div>
 </template>
 
 <script>
