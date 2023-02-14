b9bd9d90b4594e9286a338958a972ed6435f431f
msy <805109058@qq.com>
2023-02-14 22:33:50 +0800

    nprogress

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ Vue.config.productionTip = false
 //三级联动全局组件
 import TypeNav from '@/pages/Home/TypeNav'
 Vue.component(TypeNav.name,TypeNav)
+
+import {reqCategoryList} from '@/apis/index'
+reqCategoryList()
 new Vue({
   render: h => h(App),
   router,
