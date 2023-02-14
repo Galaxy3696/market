b53b3f61ddda3f7a2d33a721d762e38f3fa8ea9e
msy <805109058@qq.com>
2023-02-14 18:42:48 +0800

    Home 静态组件拆分

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 Vue.config.productionTip = false
-
+//三级联动全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+Vue.component(TypeNav.name,TypeNav)
 new Vue({
   render: h => h(App),
   router,
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,31 @@
 <template>
 <div>
-    nihao
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>          
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Brand></Brand>
 </div>
 </template>
 
 <script>
+import ListContainer from '@/pages/Home/ListContainer'
+import Reconmmend from '@/pages/Home/Recommend'
+import Rank from '@/pages/Home/Rank'
+import Like from '@/pages/Home/Like'
+import Floor from '@/pages/Home/Floor'
+import Brand from '@/pages/Home/Brand'
 export default {
-    
+    name:'home',
+    components:{
+        ListContainer,
+        Reconmmend,
+        Rank,
+        Like,
+        Floor,
+        Brand,
+    }
 }
 </script>
 
